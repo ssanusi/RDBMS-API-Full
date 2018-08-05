c57364b88188e935d313ec7d98884a947825f742
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-08-05 01:05:04 +0100

    IMPLEMENT GETDATA

--- data/db.js
+++ data/db.js
@@ -3,7 +3,18 @@ const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 module.exports = {
-
+    getUsers,
+    addUser
 }
 
 
+function getUsers(){
+    return db('users')
+}
+
+function addUser (newUser){
+    db('users').insert({
+        name : newUser,
+        createdAt : datetime('now')
+    })
+}
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
+const db = require('./data/db')
+const morgan = require('morgan')
+
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/users', (req, res) => {
+    const users = db.getUsers()
+    console.log(users)
+    users.then(
+            response => { 
+                res.status(200).json(response)
+            }
+        )
+        .catch(err => {
+            res.status(500).json({ error : err})
+        })
+});
+
+app.post('/users', (req, res) => {
+    const { name } = req.body
+    db.addUser( name )
+        .then(response => {
+            res.status(200).json(response)
+        })
+        .catch()
+});
+
+
+
 app.listen(8000, () => {
-    console.log('Example app listening on port 8000!');
+    console.log('app listening on port 8000!');
 });
 
 //Run app, then load http://localhost:8000 in a browser to see the output.
